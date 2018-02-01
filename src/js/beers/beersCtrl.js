f5a0c5e9c88e7e1435fe7447a433526834b9bfc4
SheaClose <SheaClose@gmail.com>
2018-02-01 07:49:16 -0600

    ready for lecture?

--- src/js/beers/beersCtrl.js
+++ src/js/beers/beersCtrl.js
@@ -1,4 +1,3 @@
 angular.module("app").controller("beersCtrl", function($scope, beersService) {
   $scope.beerStyleCats = ["light", "dark", "malty", "hoppy"];
-  // beersService.getBeerCategories().then(res => console.log("res: ", res));
 });
